147a07d7f4bd90903e212ffbac59677ef1f87419
Mayank Jindal <93427749+jindalmayank13@users.noreply.github.com>
2022-01-22 02:49:06 +0530

    Add files via upload

---
+++ ass2_21105028_outputfile.py
@@ -0,0 +1,68 @@
+
+Question1
+
+Part a 
+Length of Input String = 35
+
+Part b
+egaugnal evitisnes esac a si nohtyP
+
+Part c
+a case sensitive
+
+Part d
+Python is object oriented language
+
+Part e
+10
+
+Part f
+Pythonisacasesensitivelanguage
+
+=========== RESTART: /Users/mayankjindal/Python/Assignment2.py/Q2.py ===========
+Question 2
+
+Hey, Mayank Jindal here! 
+My SID is 21105028
+I am from ECE and my CGPA is 10
+
+=========== RESTART: /Users/mayankjindal/Python/Assignment2.py/Q3.py ===========
+Question 3
+ a&b :  8
+ a|b :  58
+ a^b :  50
+a<<2 :  224 	b<<2 : 40
+a>>2 :  14 	b>>4 : 0
+
+=========== RESTART: /Users/mayankjindal/Python/Assignment2.py/Q4.py ===========
+Question 4
+Enter 1st no. : 5
+Enter 2nd no. : 6
+Enter 3rd no. : 7
+Highest no. = 7
+
+=========== RESTART: /Users/mayankjindal/Python/Assignment2.py/Q5.py ===========
+Qustion 5
+Enter input string: hello name
+Yes
+
+=========== RESTART: /Users/mayankjindal/Python/Assignment2.py/Q5.py ===========
+Qustion 5
+Enter input string: hello
+No
+
+=========== RESTART: /Users/mayankjindal/Python/Assignment2.py/Q6.py ===========
+Question 6
+
+Enter 1st length : 12
+Enter 2nd length : 13
+Enter 3rd length : 5
+Yes
+
+=========== RESTART: /Users/mayankjindal/Python/Assignment2.py/Q6.py ===========
+Question 6
+
+Enter 1st length : 12
+Enter 2nd length : 5
+Enter 3rd length : 1
+No
